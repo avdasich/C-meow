d760320bbc57db08fc40b7011685b5a5e8369050
Hex <vahaibievvv@gmail.com>
2024-12-17 21:20:34 +0700

    Add files via upload

---
+++ lb1/start.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+	float f = 0.2;
+	printf("|%d|\n", 555);
+	printf("|%15d|\n", 555);
+	printf("|%-15d|\n", 555);
+
+	printf("World: %26d\n", 59);
+	printf("Letters: %24d\n", 1004);
+	 printf("Digits: %25d\n", 8);
+	printf("a = %f; b = %.6f \n", 0.10, f);
+	printf("a + b = %f \n", 0.1 +f);
+		
+
+}
+
